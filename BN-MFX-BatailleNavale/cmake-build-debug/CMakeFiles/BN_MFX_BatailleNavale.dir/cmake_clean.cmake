428c8aaf01300143006f1703b467e039854ac020
Mounir37 <47849542+Mounir37@users.noreply.github.com>
2019-03-07 15:52:14 +0100

    Code C

---
+++ BN-MFX-BatailleNavale/cmake-build-debug/CMakeFiles/BN_MFX_BatailleNavale.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BN_MFX_BatailleNavale.dir/main.c.obj"
+  "BN_MFX_BatailleNavale.pdb"
+  "BN_MFX_BatailleNavale.exe"
+  "BN_MFX_BatailleNavale.exe.manifest"
+  "libBN_MFX_BatailleNavale.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BN_MFX_BatailleNavale.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ BN-MFX-BatailleNavale/cmake-build-debug/CMakeFiles/BN_MFX_BatailleNavale.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for BN_MFX_BatailleNavale.
+# This may be replaced when dependencies are built.
